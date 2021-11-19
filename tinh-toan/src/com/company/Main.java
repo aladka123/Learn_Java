60523668d677a3e7c238accc0f7829f88e5e8dc9
aladka123 <93319080+aladka123@users.noreply.github.com>
2021-11-19 13:32:55 +0700

    Add files via upload

---
+++ tinh-toan/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int a, b;
+    a = 20;
+    b = 15;
+        System.out.println(a + "+" + b + "=" + (a+b));
+        System.out.println(a + "-" + b + "=" + (a-b));
+        System.out.println(a + "*" + b + "=" + (a*b));
+        System.out.println(a + "/" + b + "=" + (float) a/b);
+
+
+
+    }
+}
+
